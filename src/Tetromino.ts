8fb7ff64792d20a138eeaead9046b15958021d86
Sami Heinimo <sami.heinimo@helsinki.fi>
2026-03-19 15:15:31 +0200

    Initialize t shape

--- src/Tetromino.ts
+++ src/Tetromino.ts
@@ -4,13 +4,19 @@ export class Tetromino {
   private readonly orientations: RotatingShape[];
   private readonly index: number;
 
-  public static readonly T_SHAPE = new Tetromino(
-    RotatingShape.fromString(
+  public static readonly T_SHAPE = () => {
+    const initial = RotatingShape.fromString(
       `.T.
        TTT
        ...`
-    )
-  );
+    );
+    return new Tetromino([
+      initial,
+      initial.rotateRight(),
+      initial.rotateRight().rotateRight(),
+      initial.rotateRight().rotateRight().rotateRight(),
+    ]);
+  };
 
   constructor(orientations: RotatingShape[], index = 0) {
     this.orientations = orientations;
