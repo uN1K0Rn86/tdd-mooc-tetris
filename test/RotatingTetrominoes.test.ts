b2e48cbe3b800732412767b69e4ca5be1bee5a54
Sami Heinimo <sami.heinimo@helsinki.fi>
2026-03-19 15:41:56 +0200

    Uncomment other tests, all pass

--- test/RotatingTetrominoes.test.ts
+++ test/RotatingTetrominoes.test.ts
@@ -60,7 +60,7 @@ describe("The I shape", () => {
     );
   });
 
-  test.skip("can be rotated right/clockwise", () => {
+  test("can be rotated right/clockwise", () => {
     expect(shape.rotateRight().toString()).to.equalShape(
       `..I..
        ..I..
@@ -70,7 +70,7 @@ describe("The I shape", () => {
     );
   });
 
-  test.skip("can be rotated left/counter-clockwise", () => {
+  test("can be rotated left/counter-clockwise", () => {
     expect(shape.rotateLeft().toString()).to.equalShape(
       `..I..
        ..I..
@@ -80,7 +80,7 @@ describe("The I shape", () => {
     );
   });
 
-  test.skip("has 2 distinct orientations", () => {
+  test("has 2 distinct orientations", () => {
     expect(distinctOrientations(shape).size).to.equal(2);
   });
 });
