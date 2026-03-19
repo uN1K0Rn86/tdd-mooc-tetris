5b762ddda358d8f9aa12d8eb5bc3e99f0d89394e
Sami Heinimo <sami.heinimo@helsinki.fi>
2026-03-19 12:28:40 +0200

    Refactor drop logic to use private variable for whether there is a falling block

--- src/Board.ts
+++ src/Board.ts
@@ -2,6 +2,7 @@ export class Board {
   private cells: string[][];
   width;
   height;
+  private falling = false;
 
   constructor(width: number, height: number) {
     this.width = width;
@@ -18,11 +19,10 @@ export class Board {
   }
 
   drop(block: string) {
-    const isBoardEmpty = this.isEmpty();
-    if (isBoardEmpty) {
-      const middle = Math.floor(this.width / 2);
-      this.cells[0][middle] = block;
-    } else throw "already falling";
+    if (this.falling) throw "already falling";
+    const middle = Math.floor(this.width / 2);
+    this.cells[0][middle] = block;
+    this.falling = true;
   }
 
   tick() {
