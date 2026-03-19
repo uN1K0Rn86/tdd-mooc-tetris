a595e9dc171cbfffcc7ae3b0887f4a88e9c0e580
Sami Heinimo <sami.heinimo@helsinki.fi>
2026-03-19 15:47:24 +0200

    Configure falling tetrominoes test file

--- test/FallingTetrominoes.test.ts
+++ test/FallingTetrominoes.test.ts
@@ -1,22 +1,21 @@
-/*
 import { beforeEach, describe, test } from "vitest";
 import { expect } from "chai";
-import { Board } from "../src/Board.mjs";
-import { Tetromino } from "../src/Tetromino.mjs";
+import { Board } from "../src/Board";
+import { Tetromino } from "../src/Tetromino";
 
-function fallToBottom(board) {
+function fallToBottom(board: Board) {
   for (let i = 0; i < 10; i++) {
     board.tick();
   }
 }
 
 describe("Falling tetrominoes", () => {
-  let board;
+  let board: Board;
   beforeEach(() => {
     board = new Board(10, 6);
   });
 
-  test("start from the top middle", () => {
+  test.skip("start from the top middle", () => {
     board.drop(Tetromino.T_SHAPE);
 
     expect(board.toString()).to.equalShape(
@@ -59,4 +58,3 @@ describe("Falling tetrominoes", () => {
     );
   });
 });
-*/
