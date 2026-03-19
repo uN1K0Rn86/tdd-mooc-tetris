e141f2610991aba9b910122fed5d524a6e6b217a
Sami Heinimo <sami.heinimo@helsinki.fi>
2026-03-19 13:24:22 +0200

    Change fromstring to static method

--- src/RotatingShape.ts
+++ src/RotatingShape.ts
@@ -5,7 +5,7 @@ export class RotatingShape {
     this.cells = cells.map((row) => [...row]);
   }
 
-  fromString(str: string): RotatingShape {
+  static fromString(str: string): RotatingShape {
     const rows = str.split("\n").map((row) => row.trim().split(""));
     return new RotatingShape(rows);
   }
