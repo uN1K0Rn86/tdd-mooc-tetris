15fa0a635605358a621bb665be49f60661868b72
Sami Heinimo <sami.heinimo@helsinki.fi>
2026-03-31 10:21:41 +0300

    Add variant property to ArikaTetrominos

--- src/ArikaTetromino.ts
+++ src/ArikaTetromino.ts
@@ -2,59 +2,82 @@ import { Shape } from "./Shape";
 
 export class ArikaTetromino implements Shape {
   private readonly orientations: string[][];
+  readonly variant: string;
   private readonly index: number;
 
-  public static readonly T_SHAPE = new ArikaTetromino([
-    ["....", "TTT.", ".T..", "...."],
-    [".T..", "TT..", ".T..", "...."],
-    ["....", ".T..", "TTT.", "...."],
-    [".T..", ".TT.", ".T..", "...."],
-  ]);
+  public static readonly T_SHAPE = new ArikaTetromino(
+    [
+      ["....", "TTT.", ".T..", "...."],
+      [".T..", "TT..", ".T..", "...."],
+      ["....", ".T..", "TTT.", "...."],
+      [".T..", ".TT.", ".T..", "...."],
+    ],
+    "T"
+  );
 
-  public static readonly I_SHAPE = new ArikaTetromino([
-    ["....", "IIII", "....", "...."],
-    ["..I.", "..I.", "..I.", "..I."],
-    ["....", "IIII", "....", "...."],
-    ["..I.", "..I.", "..I.", "..I."],
-  ]);
+  public static readonly I_SHAPE = new ArikaTetromino(
+    [
+      ["....", "IIII", "....", "...."],
+      ["..I.", "..I.", "..I.", "..I."],
+      ["....", "IIII", "....", "...."],
+      ["..I.", "..I.", "..I.", "..I."],
+    ],
+    "I"
+  );
 
-  public static readonly L_SHAPE = new ArikaTetromino([
-    ["....", "LLL.", "L...", "...."],
-    ["LL..", ".L..", ".L..", "...."],
-    ["....", "..L.", "LLL.", "...."],
-    [".L..", ".L..", ".LL.", "...."],
-  ]);
+  public static readonly L_SHAPE = new ArikaTetromino(
+    [
+      ["....", "LLL.", "L...", "...."],
+      ["LL..", ".L..", ".L..", "...."],
+      ["....", "..L.", "LLL.", "...."],
+      [".L..", ".L..", ".LL.", "...."],
+    ],
+    "L"
+  );
 
-  public static readonly J_SHAPE = new ArikaTetromino([
-    ["....", "JJJ.", "..J.", "...."],
-    [".J..", ".J..", "JJ..", "...."],
-    ["....", "J...", "JJJ.", "...."],
-    [".JJ.", ".J..", ".J..", "...."],
-  ]);
+  public static readonly J_SHAPE = new ArikaTetromino(
+    [
+      ["....", "JJJ.", "..J.", "...."],
+      [".J..", ".J..", "JJ..", "...."],
+      ["....", "J...", "JJJ.", "...."],
+      [".JJ.", ".J..", ".J..", "...."],
+    ],
+    "J"
+  );
 
-  public static readonly S_SHAPE = new ArikaTetromino([
-    ["....", ".SS.", "SS..", "...."],
-    ["S...", "SS..", ".S..", "...."],
-    ["....", ".SS.", "SS..", "...."],
-    ["S...", "SS..", ".S..", "...."],
-  ]);
+  public static readonly S_SHAPE = new ArikaTetromino(
+    [
+      ["....", ".SS.", "SS..", "...."],
+      ["S...", "SS..", ".S..", "...."],
+      ["....", ".SS.", "SS..", "...."],
+      ["S...", "SS..", ".S..", "...."],
+    ],
+    "S"
+  );
 
-  public static readonly Z_SHAPE = new ArikaTetromino([
-    ["....", "ZZ..", ".ZZ.", "...."],
-    ["..Z.", ".ZZ.", ".Z..", "...."],
-    ["....", "ZZ..", ".ZZ.", "...."],
-    ["..Z.", ".ZZ.", ".Z..", "...."],
-  ]);
+  public static readonly Z_SHAPE = new ArikaTetromino(
+    [
+      ["....", "ZZ..", ".ZZ.", "...."],
+      ["..Z.", ".ZZ.", ".Z..", "...."],
+      ["....", "ZZ..", ".ZZ.", "...."],
+      ["..Z.", ".ZZ.", ".Z..", "...."],
+    ],
+    "Z"
+  );
 
-  public static readonly O_SHAPE = new ArikaTetromino([
-    ["....", ".OO.", ".OO.", "...."],
-    ["....", ".OO.", ".OO.", "...."],
-    ["....", ".OO.", ".OO.", "...."],
-    ["....", ".OO.", ".OO.", "...."],
-  ]);
+  public static readonly O_SHAPE = new ArikaTetromino(
+    [
+      ["....", ".OO.", ".OO.", "...."],
+      ["....", ".OO.", ".OO.", "...."],
+      ["....", ".OO.", ".OO.", "...."],
+      ["....", ".OO.", ".OO.", "...."],
+    ],
+    "O"
+  );
 
-  constructor(orientations: string[][], index = 0) {
+  constructor(orientations: string[][], variant: string, index = 0) {
     this.orientations = orientations;
+    this.variant = variant;
     this.index = index;
   }
 
